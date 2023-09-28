4a8133134ae4d33eeba7d1b3e988035f50d12c67
Samit Koyom <samitkoyom@gmail.com>
2023-09-28 10:45:48 +0700

    update fireabase_service

--- lib/functions/firebase_services.dart
+++ lib/functions/firebase_services.dart
@@ -1,70 +1,42 @@
 // ignore_for_file: avoid_print, prefer_const_constructors, no_leading_underscores_for_local_identifiers, unused_field, override_on_non_overriding_member
 
-import 'package:android_ios_noti/success.dart';
-import 'package:android_ios_noti/welcome.dart';
+// import 'package:android_ios_noti/success.dart';
+// import 'package:android_ios_noti/welcome.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// Firebase Messaging Handler for Background
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  if (message.notification != null) {
-    print(
-        'onBackgroundMessage: ${message.notification!.body} \n ${message.data}');
-    // Handle the background message here if needed.
-  }
-}
-
 class FirebaseService {
-
-  // Get the context
-  static BuildContext? _context;
 
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
+  // Callbacks for handling messages
+  Function(RemoteMessage)? onMessageCallback;
+  Function(RemoteMessage)? onMessageOpenedAppCallback;
+
+  FirebaseService({
+    this.onMessageCallback,
+    this.onMessageOpenedAppCallback,
+  });
+
   // Initialize Firebase Messaging
-  Future<void> initializeFirebaseMessaging() async {
+ Future<Map<String, Object?>> initializeFirebaseMessaging() async {
     FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-
-    // Get the device token
     String? token = await _firebaseMessaging.getToken();
-    print('Firebase token: $token');
-
-    // Request notification permission
     NotificationSettings notificationSettings =
         await _firebaseMessaging.requestPermission();
-    print(
-        'Notification authorization status: ${notificationSettings.authorizationStatus}');
-
-    // Initialize local notifications
-    await _initializeLocalNotifications();
-
-    // Set up handlers for different message types
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      if (message.notification != null) {
-        print('onMessage: ${message.notification!.body} \n ${message.data}');
-        // Show a local notification
-        _showLocalNotification(message.notification!.title!,
-            message.notification!.body!, message.data['page']);
-      }
-    });
-
-    // Firebase Messaging Handler for Background
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-    // Set up handlers for interaction with the message
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      if (message.notification != null) {
-        print(
-            'onMessageOpenedApp: ${message.notification!.body}\n ${message.data}');
-        // You can handle the message when the app is opened from a notification here.
-      }
-    });
+    NotificationResponse? tappedNotificationResponse =
+        await _initializeLocalNotifications();
+    return {
+      'token': token,
+      'authorizationStatus': notificationSettings.authorizationStatus,
+      'tappedNotificationResponse': tappedNotificationResponse,
+    };
   }
 
   // Initialize Local Notifications
-  Future<void> _initializeLocalNotifications() async {
+  Future<NotificationResponse?> _initializeLocalNotifications() async {
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('ic_launcher');
     final InitializationSettings initializationSettings =
@@ -72,30 +44,23 @@ class FirebaseService {
       android: initializationSettingsAndroid,
       iOS: DarwinInitializationSettings(),
     );
+    NotificationResponse? tappedNotificationResponse;
     await flutterLocalNotificationsPlugin.initialize(
       initializationSettings,
       onDidReceiveNotificationResponse:
           (NotificationResponse notificationResponse) async {
-        // Handle notification tapped logic here
         if (notificationResponse.notificationResponseType ==
             NotificationResponseType.selectedNotification) {
-          print(
-              'Notification forground tapped ${notificationResponse.payload}');
-          if (notificationResponse.payload == "page5") {
-            // Navigation to success page with material page route
-            Navigator.push(
-              _context!,
-              MaterialPageRoute(builder: (context) => const SuccessPage()),
-            );
-          }
+          tappedNotificationResponse = notificationResponse;
         }
       },
       onDidReceiveBackgroundNotificationResponse: null,
     );
+    return tappedNotificationResponse;
   }
 
   // Show a local notification
-  Future<void> _showLocalNotification(
+  Future<void> showLocalNotification(
        String title, String body, String data) async {
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
         AndroidNotificationDetails(
@@ -116,4 +81,25 @@ class FirebaseService {
         platformChannelSpecifics,
         payload: data);
   }
+
+  // Handle Message Method onMessage
+  void handleOnMessage(RemoteMessage message) {
+    if (message.notification != null) {
+      // print('onMessage: ${message.notification!.body} \n ${message.data}');
+      if (onMessageCallback != null) {
+        onMessageCallback!(message);
+      }
+    }
+  }
+
+  // Handle Message Method onMessageOpenedApp
+  void handleOnMessageOpenedApp(RemoteMessage message) {
+    if (message.notification != null) {
+      // print('onMessageOpenedApp: ${message.notification!.body}\n ${message.data}');
+      if (onMessageOpenedAppCallback != null) {
+        onMessageOpenedAppCallback!(message);
+      }
+    }
+  }
+
 }
